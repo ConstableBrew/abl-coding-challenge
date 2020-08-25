77fbb397420b526feee939efc47c9e8deafbae97
ConstableBrew <constablebrew@gmail.com>
2020-08-25 01:41:43 -0700

    Prepping to refactor with react and redux

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -34,4 +35,5 @@ module.exports = {
         aggregateTimeout: 250,
         ignored: [/node_modules/, /public/, /coverage/, /\.test\.js/],
     },
+    plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src/index.html') })]
 };
